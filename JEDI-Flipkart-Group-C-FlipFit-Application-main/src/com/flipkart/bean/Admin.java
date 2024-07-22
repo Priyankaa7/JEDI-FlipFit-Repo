1903685aa16ca7e02f3a73f1837435f82cd0b4bc
shabnamperween <103578339+shabnamperween@users.noreply.github.com>
2024-07-22 14:41:44 +0530

    Update Admin.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/Admin.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/Admin.java
@@ -1,18 +1,33 @@
 package com.flipkart.bean;
 
+// Class representing an Admin
 public class Admin {
-	private int adminId;
-	private String password;
-	public int getAdminId() {
-		return adminId;
-	}
-	public void setAdminId(int adminId) {
-		this.adminId = adminId;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-}
+    
+    // Private member variable to store admin ID
+    private int adminId;
+    
+    // Private member variable to store password
+    private String password;
+    
+    // Getter method for retrieving admin ID
+    public int getAdminId() {
+        return adminId;
+    }
+    
+    // Setter method for setting admin ID
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+    
+    // Getter method for retrieving password
+    public String getPassword() {
+        return password;
+    }
+    
+    // Setter method for setting password
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+} // end of Admin class
+
