edd148e62de9e63ec1ff9121fbe60b185a89fd51
Revathi Annapurna Kantipudi <91620112+RevathiKantipudi@users.noreply.github.com>
2024-07-22 14:57:53 +0530

    Update VerificationFailedException.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/exception/VerificationFailedException.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/exception/VerificationFailedException.java
@@ -1,8 +1,12 @@
 package com.flipkart.exception;
 
+// Class declaration - defines a custom exception named VerificationFailedException
+// that extends the built-in Exception class
+
 public class VerificationFailedException extends Exception{
     @Override
     public String getMessage(){
+        // This method returns a custom error message when the exception is thrown
         return "Verification Failed.";
     }
 }
