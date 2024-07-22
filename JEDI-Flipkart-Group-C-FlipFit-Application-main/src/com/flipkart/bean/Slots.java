b16d65e5160a3096993409c7dc763b38ed22ef9d
shabnamperween <103578339+shabnamperween@users.noreply.github.com>
2024-07-22 14:48:08 +0530

    Update Slots.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/Slots.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/Slots.java
@@ -1,38 +1,72 @@
 package com.flipkart.bean;
 
+/**
+ * Represents a time slot entity with details such as slot ID, time slot description,
+ * and seat count.
+ */
 public class Slots {
 
-	private int slotsId;
-	private String timeSlot; // Changed to String
-	private int seatCount;
+    private int slotsId;        // Unique identifier for the time slot
+    private String timeSlot;    // Description of the time slot (e.g., "9:00 AM - 10:00 AM")
+    private int seatCount;      // Number of seats available in the time slot
 
-	public Slots(int slotsId, String timeSlot, int seatCount) {
-		this.setSlotsId(slotsId);
-		this.setTimeSlot(timeSlot);
-		this.setSeatCount(seatCount);
-	}
+    /**
+     * Constructor to initialize a Slots object with specified values.
+     * @param slotsId The unique identifier for the time slot.
+     * @param timeSlot The description of the time slot.
+     * @param seatCount The number of seats available in the time slot.
+     */
+    public Slots(int slotsId, String timeSlot, int seatCount) {
+        this.setSlotsId(slotsId);
+        this.setTimeSlot(timeSlot);
+        this.setSeatCount(seatCount);
+    }
 
-	public int getSlotsId() {
-		return slotsId;
-	}
+    /**
+     * Getter method for retrieving the slot ID.
+     * @return The slot ID.
+     */
+    public int getSlotsId() {
+        return slotsId;
+    }
 
-	public void setSlotsId(int slotsId) {
-		this.slotsId = slotsId;
-	}
+    /**
+     * Setter method for setting the slot ID.
+     * @param slotsId The slot ID to set.
+     */
+    public void setSlotsId(int slotsId) {
+        this.slotsId = slotsId;
+    }
 
-	public String getTimeSlot() {
-		return timeSlot;
-	}
+    /**
+     * Getter method for retrieving the description of the time slot.
+     * @return The description of the time slot.
+     */
+    public String getTimeSlot() {
+        return timeSlot;
+    }
 
-	public void setTimeSlot(String timeSlot) {
-		this.timeSlot = timeSlot;
-	}
+    /**
+     * Setter method for setting the description of the time slot.
+     * @param timeSlot The description of the time slot to set.
+     */
+    public void setTimeSlot(String timeSlot) {
+        this.timeSlot = timeSlot;
+    }
 
-	public int getSeatCount() {
-		return seatCount;
-	}
+    /**
+     * Getter method for retrieving the number of seats available in the time slot.
+     * @return The number of seats available.
+     */
+    public int getSeatCount() {
+        return seatCount;
+    }
 
-	public void setSeatCount(int seatCount) {
-		this.seatCount = seatCount;
-	}
+    /**
+     * Setter method for setting the number of seats available in the time slot.
+     * @param seatCount The number of seats available to set.
+     */
+    public void setSeatCount(int seatCount) {
+        this.seatCount = seatCount;
+    }
 }
