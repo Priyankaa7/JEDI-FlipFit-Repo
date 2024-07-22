1ee7cd9132c549aff0cbf26a3476822fcb360f95
Akanksha Mishra <akankshamishra22122001@gmail.com>
2024-07-22 15:08:31 +0530

    Update GymOwnerDAOInterface.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/dao/GymOwnerDAOInterface.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/dao/GymOwnerDAOInterface.java
@@ -1,11 +1,22 @@
+/*
+ * GymOwnerDAOInterface
+ * 
+ * defines methods for interacting with the database to manage Gym Owner operations
+ */
+     
 package com.flipkart.dao;
 
 import com.flipkart.bean.GymOwner;
 
 public interface GymOwnerDAOInterface {
+      // Insert a new Gym Owner record into the database
     void newGymOwner(GymOwner gymOwner);
+    // Verify the password of a Gym Owner against stored credentials
     boolean verifyGymOwnerPassword(String email, String password);
+    // Update the status of a Gym Owner's profile to 'Pending' for approval
     boolean sendProfileForApproval(String ownerId);
+     // Update the password of a Gym Owner in the database
     void updateGymOwnerPassword(String email, String password, String updatedPassword);
+     // Retrieve the owner ID associated with a Gym Owner's email
     String getOwnerIdByEmail(String email);  // New method to fetch ownerId by email
 }
