3be4f7e75d9f2d6f29dcae56d98488aaeda7d099
Revathi Annapurna Kantipudi <91620112+RevathiKantipudi@users.noreply.github.com>
2024-07-22 14:53:47 +0530

    Update RegistrationFailedException.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/exception/RegistrationFailedException.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/exception/RegistrationFailedException.java
@@ -1,8 +1,12 @@
 package com.flipkart.exception;
 
+// Class declaration - defines a custom exception named RegistrationFailedException
+// that extends the built-in Exception class
+
 public class RegistrationFailedException extends Exception{
     @Override
     public String getMessage(){
+        // This method returns a custom error message when the exception is thrown
         return "Registration Failed. Please try again";
     }
 }
