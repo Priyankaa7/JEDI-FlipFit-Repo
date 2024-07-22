d285ec4fd019d42083f3ba0fe3801b80f8558c35
shabnamperween <103578339+shabnamperween@users.noreply.github.com>
2024-07-22 14:49:56 +0530

    Update User.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/User.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/User.java
@@ -1,58 +1,111 @@
 package com.flipkart.bean;
 
+/**
+ * Represents a User entity with basic information such as user ID, username,
+ * phone number, address, email, and password.
+ */
 public class User {
-	private int userId;
-	private String userName;
-	private String phoneNumber;
-	private String address;
-	private String email;
-	private String password;
 
-	public int getuserId() {
-		return userId;
-	}
+    private int userId;             // Unique identifier for the user
+    private String userName;        // Username of the user
+    private String phoneNumber;     // Phone number of the user
+    private String address;         // Address of the user
+    private String email;           // Email address of the user
+    private String password;        // Password for the user's account
 
-	public void setuserId(int userId) {
-		this.userId = userId;
-	}
+    /**
+     * Getter method for retrieving the user ID.
+     * @return The user ID.
+     */
+    public int getUserId() {
+        return userId;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    /**
+     * Setter method for setting the user ID.
+     * @param userId The user ID to set.
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    /**
+     * Getter method for retrieving the username.
+     * @return The username.
+     */
+    public String getUserName() {
+        return userName;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    /**
+     * Setter method for setting the username.
+     * @param userName The username to set.
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    /**
+     * Getter method for retrieving the email address of the user.
+     * @return The email address of the user.
+     */
+    public String getEmail() {
+        return email;
+    }
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+    /**
+     * Setter method for setting the email address of the user.
+     * @param email The email address to set.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    /**
+     * Getter method for retrieving the phone number of the user.
+     * @return The phone number of the user.
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    /**
+     * Setter method for setting the phone number of the user.
+     * @param phoneNumber The phone number to set.
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    /**
+     * Getter method for retrieving the address of the user.
+     * @return The address of the user.
+     */
+    public String getAddress() {
+        return address;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    /**
+     * Setter method for setting the address of the user.
+     * @param address The address to set.
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    /**
+     * Getter method for retrieving the password of the user's account.
+     * @return The password of the user's account.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Setter method for setting the password of the user's account.
+     * @param password The password to set.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
