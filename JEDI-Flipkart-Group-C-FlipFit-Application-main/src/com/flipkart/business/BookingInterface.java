6d9d26f34bf5b7e22741f31fc178872a256dca83
SravanthiSs <69337593+SravanthiSs@users.noreply.github.com>
2024-07-22 14:53:44 +0530

    Update BookingInterface.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/business/BookingInterface.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/business/BookingInterface.java
@@ -1,7 +1,8 @@
-package com.flipkart.business;
-
+// BookingInterface defines the methods related to booking operations
 public interface BookingInterface {
 
-	public void addBooking (String userId, String slotId, String gymId);
-	public void cancelBooking (String bookingId);
+    // Method to add a booking for a user with specified slot and gym
+    public void addBooking(String userId, String slotId, String gymId);
+    // Method to cancel a booking based on booking ID
+    public void cancelBooking(String bookingId);
 }
