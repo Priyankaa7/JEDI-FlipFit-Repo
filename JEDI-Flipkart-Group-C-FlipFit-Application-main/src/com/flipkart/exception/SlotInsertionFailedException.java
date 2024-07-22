9121adf766ea81738e257a295a3a29bc6d693532
Revathi Annapurna Kantipudi <91620112+RevathiKantipudi@users.noreply.github.com>
2024-07-22 14:54:41 +0530

    Update SlotInsertionFailedException.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/exception/SlotInsertionFailedException.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/exception/SlotInsertionFailedException.java
@@ -1,8 +1,12 @@
 package com.flipkart.exception;
 
+// Class declaration - defines a custom exception named SlotInsertionFailedException
+// that extends the built-in Exception class
+
 public class SlotInsertionFailedException extends Exception{
     @Override
     public String getMessage(){
+        //This method returns a custom error message when the exception is thrown
         return "Slot Insertion Failed";
     }
 }
