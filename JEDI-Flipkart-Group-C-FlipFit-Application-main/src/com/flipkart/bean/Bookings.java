58a1da25c36e361f5130914e4a3126cf9ba2d84d
shabnamperween <103578339+shabnamperween@users.noreply.github.com>
2024-07-22 14:42:43 +0530

    Update Bookings.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/Bookings.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/Bookings.java
@@ -3,68 +3,52 @@ package com.flipkart.bean;
 import java.sql.Date; // Import Date class
 import java.sql.Time; // Import Time class
 
+// Class representing a booking entity
 public class Bookings {
-	private int bookingId;
-	private int userId;
-	private Date date; // Changed to Date
-	private Time time; // Changed to Time
-	private String slotId;
-	private int gymId;
-	private String status;
 
-	public Date getDate() { // Changed to Date
-		return date;
-	}
+    // Private member variables to store booking details
+    private int bookingId;
+    private int userId;
+    private Date date; // Date of the booking
+    private Time time; // Time of the booking
+    private String slotId; // Slot ID for the booking
+    private int gymId; // ID of the gym where the booking is made
+    private String status; // Status of the booking (e.g., confirmed, canceled)
 
-	public void setDate(Date date) { // Changed to Date
-		this.date = date;
-	}
+    // Getter method for retrieving the booking date
+    public Date getDate() {
+        return date;
+    }
 
-	public String getSlotId() {
-		return slotId;
-	}
+    // Setter method for setting the booking date
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	public void setSlotId(String slotId) {
-		this.slotId = slotId;
-	}
+    // Getter method for retrieving the slot ID
+    public String getSlotId() {
+        return slotId;
+    }
 
-	public int getGymId() {
-		return gymId;
-	}
+    // Setter method for setting the slot ID
+    public void setSlotId(String slotId) {
+        this.slotId = slotId;
+    }
 
-	public void setGymId(int gymId) {
-		this.gymId = gymId;
-	}
+    // Getter method for retrieving the gym ID
+    public int getGymId() {
+        return gymId;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    // Setter method for setting the gym ID
+    public void setGymId(int gymId) {
+        this.gymId = gymId;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    // Getter method for retrieving the booking status
+    public String getStatus() {
+        return status;
+    }
 
-	public Time getTime() { // Changed to Time
-		return time;
-	}
-
-	public void setTime(Time time) { // Changed to Time
-		this.time = time;
-	}
-
-	public int getBookingId() {
-		return bookingId;
-	}
-
-	public void setBookingId(int bookingId) {
-		this.bookingId = bookingId;
-	}
-
-	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-}
+    // Setter method for setting the booking status
+    public void 
