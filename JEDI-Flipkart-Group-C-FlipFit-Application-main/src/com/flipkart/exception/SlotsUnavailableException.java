84ea95709b92d91b354ec106bb65c64ee8b1a90d
Revathi Annapurna Kantipudi <91620112+RevathiKantipudi@users.noreply.github.com>
2024-07-22 14:57:22 +0530

    Update SlotsUnavailableException.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/exception/SlotsUnavailableException.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/exception/SlotsUnavailableException.java
@@ -1,8 +1,12 @@
 package com.flipkart.exception;
 
+// Class declaration - defines a custom exception named SlotsUnavailableException
+// that extends the built-in Exception class
+
 public class SlotsUnavailableException extends Exception{
     @Override
     public String getMessage(){
+        // This method returns a custom error message when the exception is thrown
         return "Slots unavailable. Please try another gym or different slot in the gym";
     }
 }
