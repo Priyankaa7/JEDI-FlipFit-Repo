c33e88886ecdfaff17e7566d6e81b85a8fd04534
SravanthiSs <69337593+SravanthiSs@users.noreply.github.com>
2024-07-22 14:52:33 +0530

    Update AdminService.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/business/AdminService.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/business/AdminService.java
@@ -50,6 +50,48 @@ public class AdminService implements AdminInterface {
 
 }*/
 
+// public class AdminService implements AdminInterface {
+
+// 	AdminDAOInterface adminDaoInterface = new AdminDAOImplementation();
+// 	Scanner obj = new Scanner(System.in);
+
+// 	@Override
+// 	public void viewGymOwners() {
+// 		adminDaoInterface.viewGymOwners();
+// 	}
+
+// 	@Override
+// 	public void viewGyms() {
+// 		adminDaoInterface.viewGyms();
+// 	}
+
+// 	@Override
+// 	public void viewUsers() {
+// 		adminDaoInterface.viewUsers();
+// 	}
+
+// 	@Override
+// 	public void verifyGym(int gymId) {
+// 		adminDaoInterface.verifyGyms(gymId);
+// 	}
+
+// 	@Override
+// 	public void verifyGymOwner(int gymOwnerId) {
+// 		adminDaoInterface.verifyGymOwners(gymOwnerId);
+// 	}
+
+// 	@Override
+// 	public List<GymOwner> getUnverifiedGymOwners() {
+// 		return adminDaoInterface.getUnverifiedGymOwner();
+// 	}
+
+// 	@Override
+// 	public List<Gym> getUnverifiedGyms() {
+// 		return adminDaoInterface.getUnverifiedGyms();
+// 	}
+// }
+
+
 
 package com.flipkart.business;
 
@@ -61,43 +103,46 @@ import com.flipkart.bean.GymOwner;
 import com.flipkart.dao.AdminDAOImplementation;
 import com.flipkart.dao.AdminDAOInterface;
 
+// AdminService class implements the AdminInterface and provides functionality for admin operations
 public class AdminService implements AdminInterface {
 
-	AdminDAOInterface adminDaoInterface = new AdminDAOImplementation();
-	Scanner obj = new Scanner(System.in);
+	// Instance variables
+	AdminDAOInterface adminDaoInterface = new AdminDAOImplementation(); // Using AdminDAOImplementation for data access
+	Scanner obj = new Scanner(System.in); // Scanner object for user input
 
 	@Override
 	public void viewGymOwners() {
-		adminDaoInterface.viewGymOwners();
+		adminDaoInterface.viewGymOwners(); // Delegates to AdminDAOInterface for viewing gym owners
 	}
 
 	@Override
 	public void viewGyms() {
-		adminDaoInterface.viewGyms();
+		adminDaoInterface.viewGyms(); // Delegates to AdminDAOInterface for viewing gyms
 	}
 
 	@Override
 	public void viewUsers() {
-		adminDaoInterface.viewUsers();
+		adminDaoInterface.viewUsers(); // Delegates to AdminDAOInterface for viewing users (not implemented in provided DAO)
 	}
 
 	@Override
 	public void verifyGym(int gymId) {
-		adminDaoInterface.verifyGyms(gymId);
+		adminDaoInterface.verifyGyms(gymId); // Delegates to AdminDAOInterface for verifying a gym by ID
 	}
 
 	@Override
 	public void verifyGymOwner(int gymOwnerId) {
-		adminDaoInterface.verifyGymOwners(gymOwnerId);
+		adminDaoInterface.verifyGymOwners(gymOwnerId); // Delegates to AdminDAOInterface for verifying a gym owner by ID
 	}
 
 	@Override
 	public List<GymOwner> getUnverifiedGymOwners() {
-		return adminDaoInterface.getUnverifiedGymOwner();
+		return adminDaoInterface.getUnverifiedGymOwner(); // Retrieves unverified gym owners using AdminDAOInterface
 	}
 
 	@Override
 	public List<Gym> getUnverifiedGyms() {
-		return adminDaoInterface.getUnverifiedGyms();
+		return adminDaoInterface.getUnverifiedGyms(); // Retrieves unverified gyms using AdminDAOInterface
 	}
 }
+
