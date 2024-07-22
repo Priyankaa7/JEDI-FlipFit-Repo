221ae8cbb8056f2a8d7063ae9cd691a127701c7a
Palak Garg <palakgarg296@gmail.com>
2024-07-22 15:01:06 +0530

    Update GymOwnerService.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/business/GymOwnerService.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/business/GymOwnerService.java
@@ -10,51 +10,62 @@ import com.flipkart.dao.GymOwnerDAOImplementation;
 
 import java.util.List;
 
+// Service class implementing GymOwnerInterface
 public class GymOwnerService implements GymOwnerInterface {
 
+	// DAO instances to interact with the database
 	GymOwnerDAOInterface gymOwnerDAO = new GymOwnerDAOImplementation();
 	GymDAOInterface gymDAO = new GymDAOImplementation();
 
+	//Method to validate the login credentials of a gym owner.
 	@Override
 	public boolean validateLogin(String email, String password) {
 		return gymOwnerDAO.verifyGymOwnerPassword(email, password);
 	}
 
+	//Method to add a gym along with its slots.
 	@Override
 	public void addGymWithSlots(Gym gym) {
 		gymDAO.addGym(gym);
 	}
 
+	//Method to retrieve a list of gyms owned by a specific user.
 	@Override
 	public List<Gym> viewMyGyms(String ownerId) {
 		return gymDAO.getGymsByOwnerId(ownerId);
 	}
 
+	//Method to create a new gym owner profile.
 	@Override
 	public void createGymOwner(GymOwner gymOwner) {
 		gymOwnerDAO.newGymOwner(gymOwner);
 	}
-
+	
+	//Method to verify the password of a gym owner.
 	@Override
 	public boolean verifyGymOwnerPassword(String email, String password) {
 		return false;
 	}
 
+	//Method to update the password of a gym owner.
 	@Override
 	public void updateGymOwnerPassword(String email, String password, String updatedPassword) {
 
 	}
 
+	//Method to add slots to a gym.
 	@Override
 	public void addSlots(int gymId, List<Slots> slots) {
 		gymDAO.insertSlots(slots, gymId);
 	}
 
+	//Method to send a gym owner profile for approval.
 	@Override
 	public boolean sendProfileForApproval(String ownerId) {
 		return gymOwnerDAO.sendProfileForApproval(ownerId);
 	}
 
+	//Method to get the owner ID by email.
 	@Override
 	public String getOwnerIdByEmail(String email) {
 		return gymOwnerDAO.getOwnerIdByEmail(email);
