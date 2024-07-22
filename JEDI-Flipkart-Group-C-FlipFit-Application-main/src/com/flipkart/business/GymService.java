6e42b1ab2baccc8a2335e462c7c7693ede186e88
Palak Garg <palakgarg296@gmail.com>
2024-07-22 15:02:42 +0530

    Update GymService.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/business/GymService.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/business/GymService.java
@@ -6,12 +6,16 @@ import com.flipkart.dao.GymDAOImplementation;
 
 import java.util.List;
 
+// Service class implementing GymInterface
 public class GymService implements GymInterface {
 
+    // DAO instance to interact with the database
     private GymDAOInterface gymDAO = new GymDAOImplementation();
 
+    // Method to retrieve a list of gyms owned by a specific owner.
     @Override
     public List<Gym> getGymsByOwnerId(String ownerId) {
+        // Delegate the call to the DAO layer
         return gymDAO.getGymsByOwnerId(ownerId);
     }
 }
