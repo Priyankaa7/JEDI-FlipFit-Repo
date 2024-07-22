1083f6478b3ad0ff69cb8be55963a889cae630d4
shabnamperween <103578339+shabnamperween@users.noreply.github.com>
2024-07-22 14:43:33 +0530

    Update Customer.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/Customer.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/bean/Customer.java
@@ -1,22 +1,30 @@
 package com.flipkart.bean;
 
+// Customer class inheriting from User class
 public class Customer extends User {
-    private int membershipId;
-    private String membershipType;
 
+    // Private member variables specific to Customer
+    private int membershipId; // Unique ID for customer's membership
+    private String membershipType; // Type of membership (e.g., Gold, Silver)
+
+    // Getter method for retrieving membership ID
     public int getMembershipId() {
         return membershipId;
     }
 
+    // Setter method for setting membership ID
     public void setMembershipId(int membershipId) {
         this.membershipId = membershipId;
     }
 
+    // Getter method for retrieving membership type
     public String getMembershipType() {
         return membershipType;
     }
 
+    // Setter method for setting membership type
     public void setMembershipType(String membershipType) {
         this.membershipType = membershipType;
     }
-}
+
+} // end of Customer class
