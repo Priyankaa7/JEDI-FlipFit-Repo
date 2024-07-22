f3a5137c5a48b37e685e9a74a1d0aaa02ad2a443
Palak Garg <palakgarg296@gmail.com>
2024-07-22 14:47:41 +0530

    added comments JDBCConnection.java

--- JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/utils/JDBCConnection.java
+++ JEDI-Flipkart-Group-C-FlipFit-Application-main/src/com/flipkart/utils/JDBCConnection.java
@@ -9,40 +9,54 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class JDBCConnection {
+    // A single instance of Connection to ensure singleton pattern
     private static Connection connection = null;
-
+    // Method to get the database connection
     public static Connection getConnection() {
         try {
+            // Check if the connection is valid and return it if so
             if (isValid()) {
                 return connection;
             } else {
                 try {
+                    // Load properties from the configuration file
                     Properties prop = new Properties();
                     InputStream inputStream = JDBCConnection.class.getClassLoader().getResourceAsStream("./config.properties");
                     prop.load(inputStream);
+
+                    // Get database connection properties
                     String driver = prop.getProperty("driver");
                     String url = prop.getProperty("url");
                     String user = prop.getProperty("user");
                     String password = prop.getProperty("password");
+                    
+                    // Load the database driver class
                     Class.forName(driver);
+                    // Establish the database connection
                     connection = DriverManager.getConnection(url, user, password);
                     System.out.println("Connected to database");
                 } catch (ClassNotFoundException e) {
+                    // Handle case where the driver class is not found
                     e.printStackTrace();
                 } catch (SQLException e) {
+                    // Handle SQL related exceptions
                     e.printStackTrace();
                 } catch (FileNotFoundException e) {
+                    // Handle case where the properties file is not found
                     e.printStackTrace();
                 } catch (IOException e) {
+                    // Handle I/O exceptions
                     e.printStackTrace();
                 }
             }
         } catch (SQLException e) {
+            // Handle SQL related exceptions
             e.printStackTrace();
             System.out.println("Connection failed");
         }
         return connection;
     }
+    // Method to check if the connection is valid
     public static boolean isValid() throws SQLException {
         return connection != null && !connection.isClosed();
     }
